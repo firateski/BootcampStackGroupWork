74612f9280d58577d0cc225f01bf2818f856d985
Fırat Eşki <frat888@gmail.com>
2020-09-24 20:27:31 +0300

    Implement 'Node' model

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Node {
+
+    private int value;
+    private Node next;
+    private Node previous;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
 }
