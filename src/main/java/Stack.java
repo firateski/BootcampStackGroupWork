bf671efe92516b493aeaf860c547bf6f7f8e94b5
Fırat Eşki <frat888@gmail.com>
2020-09-24 21:04:44 +0300

    Implement getSize method

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -3,4 +3,23 @@ import model.Node;
 import java.util.Objects;
 
 public class Stack {
+
+    private Node rootNode = null;
+
+    public int getSize() {
+        if(rootNode == null){
+            return 0;
+        }
+
+        int size = 1;
+
+        Node currentNode = rootNode;
+
+        while (!Objects.isNull(currentNode.getNext())){
+            size++;
+            currentNode = currentNode.getNext();
+        }
+
+        return size;
+    }
 }
