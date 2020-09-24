335b1ce1a170fe2b30d0e06d814dafaecebd630a
Fırat Eşki <frat888@gmail.com>
2020-09-24 21:04:01 +0300

    Create tests for getSize method

--- src/test/java/StackTests.java
+++ src/test/java/StackTests.java
@@ -1,4 +1,33 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class StackTests {
+
+    @Test
+    public void getSize_ShouldReturnZero_WhenStackIsEmpty() {
+        // Arrange
+        Stack sut = new Stack();
+
+        // Act
+        int output = sut.getSize();
+
+        // Verify
+        assertEquals(0, output);
+    }
+
+    @Test
+    public void getSize_ShouldReturnThree_WhenStackHasThreeItems() {
+        // Arrange
+        Stack sut = new Stack();
+        sut.push(1);
+        sut.push(2);
+        sut.push(3);
+
+        // Act
+        int output = sut.getSize();
+
+        // Verify
+        assertEquals(3, output);
+    }
 }
